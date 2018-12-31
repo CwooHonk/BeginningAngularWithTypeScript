588eee8b0b0445f7a50f0018e1cdc9d2963d0efa
bob <->
2018-12-31 21:55:23 +0000

    Chapter 7.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 //import { AppModule } from './app/Intro/app.module';
-import { AppModule } from './app/Forms/app.module';
+//import { AppModule } from './app/Forms/app.module';
+import { AppModule } from './app/Login/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
