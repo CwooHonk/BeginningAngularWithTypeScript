1d71a94c4138cf1e3f548d5c08cb5bbbbe6d7579
bob <->
2018-12-31 17:59:16 +0000

    Chapter 6.

---
+++ src/app/Forms/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { UserFormComponent } from './user-form.component';
+
+@Component({
+  selector: 'app-root',
+  template: '<user-form></user-form>',
+  providers: []
+})
+export class AppComponent {
+}
