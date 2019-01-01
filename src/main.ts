b2ef6e5550f548023c952fb54b4e46990346b7bd
bob <->
2019-01-01 15:38:03 +0000

    Chapter 9

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 //import { AppModule } from './app/Intro/app.module';
 //import { AppModule } from './app/Forms/app.module';
 //import { AppModule } from './app/Login/app.module';
-import { AppModule } from './app/Search/app.module';
+//import { AppModule } from './app/Search/app.module';
+import { AppModule } from './app/GitHub/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
