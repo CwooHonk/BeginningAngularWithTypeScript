123aaf6b39ba110997eca63be0c7357137d9e7d0
bob <->
2019-01-01 21:01:33 +0000

    Starting chapter 10.

--- src/app/GitHub/app.module.ts
+++ src/app/GitHub/app.module.ts
@@ -4,15 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GitHubComponent } from './github.component';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './notfound.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, NotFoundComponent, GitHubComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
