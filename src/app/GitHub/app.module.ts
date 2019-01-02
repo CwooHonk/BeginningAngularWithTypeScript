44ddcccf3a2fbc1e24802583cf6ecc9eb7e82dc3
bob <->
2019-01-02 22:40:14 +0000

    Some more on chapter 10.

--- src/app/GitHub/app.module.ts
+++ src/app/GitHub/app.module.ts
@@ -7,17 +7,19 @@ import { AppComponent } from './app.component';
 import { GitHubComponent } from './github.component';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
+import { GitHubUserComponent } from './githubuser.component';
+
 import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, NotFoundComponent, GitHubComponent
+    AppComponent, HomeComponent, NotFoundComponent, GitHubComponent, GitHubUserComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    routing
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/GitHub/app.routing.ts
+++ src/app/GitHub/app.routing.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
 import { GitHubComponent } from './github.component';
+import { GitHubUserComponent } from './githubuser.component';
 
 export const routing = RouterModule.forRoot([
     {path: '', component: HomeComponent},
     {path: 'GitHub', component: GitHubComponent},
-    {path: '**', component: NotFoundComponent}
+    {path: 'GitHub/user/:login/:score', component: GitHubUserComponent},
+    {path: '**', component: NotFoundComponent},
 ]);
